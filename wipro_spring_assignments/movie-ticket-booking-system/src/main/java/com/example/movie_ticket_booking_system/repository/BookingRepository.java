ac697c31f5bc5a9fa7a90ace859757b1813d1c55
Gowthami8420 <135121670+Gowthami8420@users.noreply.github.com>
2025-08-09 11:20:10 +0530

    Delete wipro_spring_assignments directory

--- wipro_spring_assignments/movie-ticket-booking-system/src/main/java/com/example/movie_ticket_booking_system/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.movie_ticket_booking_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.movie_ticket_booking_system.model.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-
-}
